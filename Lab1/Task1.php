0b82da0f9f8a5713a002d86864ccbaa5f71ce852
s24306 <s24306@pjwstk.edu.pl>
2023-02-27 15:35:22 +0100

    Finish Lab 1

---
+++ Lab1/Task1.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    <li><a href="https://www.google.com/">link 1</a></li>
+        <ul>
+            <li><a href="https://www.google.com/">sublink 1</a></li>
+            <li><a href="https://www.google.com/">sublink 2</a></li>
+            <li><a href="https://www.google.com/">sublink 3</a></li>
+        </ul>
+    <li><a href="https://www.google.com/">link 2</a></li>
+    <li><a href="https://www.google.com/">link 3</a></li>
+</ul>
+
+<h4>title 1</h4>
+<p>Lorem ipsum</p>
+<img src="https://ocdn.eu/pulscms-transforms/1/W1hk9kpTURBXy9hYTBkOTY0Zjc1MWRlOTdiMDhlZTM5ZjI3MWE1YWVjMS5qcGeTlQMAzEDNB9DNBGWTCaYyNTBkMDYGkwXNBLDNAnbeAAGhMAE/jan-pawel-ii.jpg" alt="Paweł">
+<h3>subtitle 1</h3>
+<p>Lorem ipsum</p>
+<h3>subtitle 2</h3>
+<p>Lorem ipsum</p>
+<h4>title 2</h4>
+<p>Lorem ipsum</p>
+
+<form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="firstname" name="firstname"><br>
+    <label for="description">Description</label><br>
+    <textarea id="description" name="description" rows="4" cols="50"></textarea><br>
+    <label for="age">Age</label><br>
+    <select name="age" id="age">
+        <option value="" disabled selected>Select your option</option>
+        <option value="18+">18+</option>
+        <option value="18-">18-</option>
+    </select><br>
+    <label for="option">Option</label><br>
+    <input type="radio" id="a" name="option" value="a"><br>
+    <input type="radio" id="b" name="option" value="b"><br>
+    <input type="radio" id="c" name="option" value="c"><br>
+    <input type="submit" value="submit"><br>
+</form>
+
+<table>
+    <tr>
+        <th>col1</th>
+        <th>col2</th>
+        <th>col3</th>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+    </tr>
+</table>
+
+</body>
+</html>
+<?php
